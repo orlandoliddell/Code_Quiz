0339535588a34e1b9e8612095bbfc3bc8d9dcefc
filthyningen <malikliddell@gmail.com>
2020-08-06 18:12:45 -0400

    Set up array for questions and answers. Added click event for making welcome display none and question container display block. Fixed script.js link.

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,7 @@
-
-
-
+var li = document.createElement("LI");
+var startButton = document.getElementById("start-button");
+var welcomeCard = document.getElementById("welcom-card");
+var questionDiv = document.getElementById("question-div")
 
 var timeInterval;
 var time = 0;
@@ -11,20 +12,33 @@ var questArrofObjIndex = 0;
 
 var questArrofObj = [
     {
+        question: "a",
         answerArr: ["a", "b", "c", "d"],
         correctId: 0
     },
     {
+        question: "a",
         answerArr: ["a", "b", "c", "d"],
         correctId: 1
     },
     {
+        question: "a",
         answerArr: ["a", "b", "c", "d"],
         correctId: 2
     },
     {
+        question: "a",
         answerArr: ["a", "b", "c", "d"],
         correctId: 3
     }
    
 ]
+
+
+
+console.log("here")
+startButton.addEventListener("click", function () {
+    console.log("start")
+    welcomeCard.setAttribute("style", "display: none !important");
+    questionDiv.setAttribute("style", "display: block !important")
+})
