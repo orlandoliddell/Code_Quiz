8df724244eb11954c9449fe4d7711872343a12cd
filthyningen <malikliddell@gmail.com>
2020-08-06 16:39:00 -0400

    Made score, and made it display none.

---
+++ Assets/script.js
@@ -0,0 +1,30 @@
+
+
+
+
+var timeInterval;
+var time = 0;
+var timeCap = 75;
+var numberCorrect = 0;
+var questArrofObjIndex = 0;
+
+
+var questArrofObj = [
+    {
+        answerArr: ["a", "b", "c", "d"],
+        correctId: 0
+    },
+    {
+        answerArr: ["a", "b", "c", "d"],
+        correctId: 1
+    },
+    {
+        answerArr: ["a", "b", "c", "d"],
+        correctId: 2
+    },
+    {
+        answerArr: ["a", "b", "c", "d"],
+        correctId: 3
+    }
+   
+]
